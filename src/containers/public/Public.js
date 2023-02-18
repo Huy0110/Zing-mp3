ae37b053f6f3dc6a17e9aa2d4ff623c38aa3ee9c
Huy0110 <duchuy01102001@gmail.com>
2023-02-18 17:28:14 +0700

    Add sidebar to public

--- src/containers/public/Public.js
+++ src/containers/public/Public.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { SidebarLeft, SidebarRight } from '../../components'
 
 const Public = () => {
   return (
-    <div>
-      Public 
-      <Outlet />
+    <div className='w-full flex overflow-y-auto'>
+      <div className='w-[240px] flex-none border border-blue-500'>
+        <SidebarLeft>
+
+        </SidebarLeft>
+      </div>
+      <div className='flex-auto border border-red-500'>
+        <Outlet />
+      </div>
+      <div>
+        <SidebarRight className='w-[329px] flex-none'></SidebarRight>
+      </div>
     </div>
   )
 }
