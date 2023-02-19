0e75e1a3bfed89e93d6bb05fc4612fa527f1c9d5
Huy0110 <duchuy01102001@gmail.com>
2023-02-19 15:11:38 +0700

    Add more icon

--- src/ultis/icons.js
+++ src/ultis/icons.js
@@ -1,6 +1,15 @@
-import {MdOutlineLibraryMusic} from 'react-icons/md'
+import {MdOutlineLibraryMusic, MdOutlineFeed} from 'react-icons/md'
+import { HiOutlineChartPie, HiArrowNarrowLeft, HiArrowNarrowRight} from 'react-icons/hi'
+import {TbChartArcs} from 'react-icons/tb'
+import {FiSearch} from 'react-icons/fi'
 const icons = {
-    MdOutlineLibraryMusic
+    MdOutlineLibraryMusic,
+    MdOutlineFeed,
+    HiOutlineChartPie,
+    TbChartArcs,
+    HiArrowNarrowLeft,
+    HiArrowNarrowRight,
+    FiSearch
 }
 
 export default icons
