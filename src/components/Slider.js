0cc60f5e4cd942f07851cf12a9b903dc9621218b
Huy0110 <duchuy01102001@gmail.com>
2023-02-22 15:36:04 +0700

    Add animation in slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -11,39 +11,49 @@ const Slider = () => {
       const intervalId = setInterval(() => {
         const list = getArrSlider(min, max, sliderEls.length - 1)
         for(let i =0; i< sliderEls.length; i++){
+          // Delete classnames (css)
+          sliderEls[i].classList.remove('animate-slide-right', 'order-last', 'z-20')
+          sliderEls[i].classList.remove('animate-slide-left', 'order-first', 'z-10')
+          sliderEls[i].classList.remove('animate-slide-left2', 'order-2', 'z-10')
+          // Hide or show image
           if(list.some(item=>item ===i)){
             sliderEls[i].style.cssText = 'display: block'
           } else {
             sliderEls[i].style.cssText = 'display: none'
           }
         }
-        if(min === sliderEls.length - 1){
-          min = 0
-        } else {
-          min +=1
-        }
-        if(max === sliderEls.length - 1){
-          max = 0
-        } else {
-          max +=1
-        }
+        // Add animation by adding classnames
+        list.forEach(item => {
+          if(item === max){
+            sliderEls[item].classList.add('animate-slide-right', 'order-last', 'z-20')
+          } else if( item === min){
+            sliderEls[item].classList.add('animate-slide-left', 'order-fist', 'z-10')
+          } else {
+            sliderEls[item].classList.add('animate-slide-left2', 'order-2', 'z-10')
+          }
+        })
+        min = (min === sliderEls.length - 1) ? 0 : min -1
+        max = (max === sliderEls.length - 1) ? 0 : max +1
         // console.log("list: ",list)
-      }, 1000)
+      }, 2000)
       return ()=> {
         intervalId && clearInterval(intervalId)
       }
     }, [])
 
   return (
-    <div className='flex gap-4 w-full overflow-hidden px-[59px] pt-8'>
-        {banner?.map(item => (
+    <div className='w-fill overflow-hidden px-[59px]'>
+
+    <div className='flex gap-8 w-full pt-8'>
+        {banner?.map((item, index) => (
             // eslint-disable-next-line jsx-a11y/alt-text
             <img 
                 key={item.encodeId}
                 src={item.banner}
-                className='slider-items flex-1 object-contain w-1/3 rounded-lg'
+                className={`slider-items flex-1 object-contain w-[30%] rounded-lg ${index <=2 ? 'block': 'hidden'}`}
             />
         ))}
+    </div>
     </div>
   )
 }
