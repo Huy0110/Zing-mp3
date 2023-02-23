d476dad1c736b8a9c978627ff0813aee83da9fc6
Huy0110 <duchuy01102001@gmail.com>
2023-02-23 16:38:39 +0700

    Add icons

--- src/ultis/icons.js
+++ src/ultis/icons.js
@@ -1,7 +1,10 @@
-import {MdOutlineLibraryMusic, MdOutlineFeed} from 'react-icons/md'
+import {MdOutlineLibraryMusic, MdOutlineFeed, MdSkipNext, MdSkipPrevious} from 'react-icons/md'
 import { HiOutlineChartPie, HiArrowNarrowLeft, HiArrowNarrowRight} from 'react-icons/hi'
 import {TbChartArcs} from 'react-icons/tb'
 import {FiSearch} from 'react-icons/fi'
+import {AiFillHeart, AiOutlineHeart} from 'react-icons/ai'
+import {BsThreeDots, BsFillPlayFill, BsPauseFill} from 'react-icons/bs'
+import {CiRepeat, CiShuffle} from 'react-icons/ci'
 const icons = {
     MdOutlineLibraryMusic,
     MdOutlineFeed,
@@ -9,7 +12,16 @@ const icons = {
     TbChartArcs,
     HiArrowNarrowLeft,
     HiArrowNarrowRight,
-    FiSearch
+    FiSearch,
+    AiFillHeart,
+    AiOutlineHeart,
+    BsThreeDots,
+    CiRepeat,
+    MdSkipNext,
+    MdSkipPrevious,
+    CiShuffle,
+    BsFillPlayFill,
+    BsPauseFill
 }
 
 export default icons
